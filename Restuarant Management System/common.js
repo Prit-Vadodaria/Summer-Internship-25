ce2fb4943b9e61180e272c35fed068200b17d909
Prit-Vadodaria <pritvadodaria16@gmail.com>
2025-07-28 21:27:59 +0530

    Add files via upload

---
+++ Restuarant Management System/common.js
@@ -0,0 +1,8 @@
+
+let food = [
+            { name: 'Pizza', price: 10,discription: 'Delicious cheese pizza' },
+            { name: 'Salad', price: 5, discription: 'Fresh garden salad' },
+            { name: 'Burger', price: 8 ,discription: 'Juicy cheese burger' },
+            { name: 'Pasta', price: 12 ,discription: 'Creamy Alfredo pasta' },
+            { name: 'Sushi', price: 15 ,discription: 'Assorted sushi platter' }
+        ];
